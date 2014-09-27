1ec87f062b6ae82bf7a4a7a4dc71ac39eda9d1ab
Lellansin <lellansin@gmail.com>
2014-09-27 20:58:08 +0800

    update

---
+++ redo/src/scan/scan.c
@@ -0,0 +1,17 @@
+
+
+int scan(int argc, char const *argv[])
+{
+	// todo hide detail
+
+	// deal with file
+	if (argc > 1)
+	{
+		// read file, and return the file
+	}
+
+	// deal with repl
+
+
+	return 0;
+}

abe0d6f44b54d774a1305a2bda275f8a000606de
Lellansin <lellansin@gmail.com>
2014-09-27 22:51:24 +0800

    添加接口

---
+++ redo/src/parse/parser.c
@@ -0,0 +1,19 @@
+
+#include "parser.h"
+
+int parse(int argc, char const *argv[])
+{
+	/* 将一段字符串，比如 (print "hello world")
+		处理为 
+		token[0] print
+		token[1] "hello world"
+
+		如 (print (add 1 2))
+		处理为 
+		token[0] print
+		token[1] (add 1 2)
+
+	*/
+
+	return 0;
+}

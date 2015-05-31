897349e1f34eccccc98c00318d50ef0eb1f8c7b5
Lellansin <lellansin@gmail.com>
2015-05-31 13:59:06 +0800

    add increase and decrease

--- src/js/lib/arithmetic.js
+++ src/js/lib/arithmetic.js
@@ -1,3 +1,44 @@
+var TYPE = require('../config/type');
+
+ /*
+  * increase
+  */
+exports.inc = function(params) {
+	if (params.length != 1)
+		throw new Error('function \"inc\" only take one parameter');
+
+	var value = params[0],
+		num = value.get() + 1;
+
+	if (typeof num == 'number')
+		value.set(num);
+	else
+		throw new Error('type ' + value.type + ' increase not support');
+
+	return num;
+};
+
+/*
+ * decrease
+ */
+exports.dec = function(params) {
+	if (params.length != 1)
+		throw new Error('function \"inc\" only take one parameter');
+
+	var value = params[0],
+		num = value.get() - 1;
+
+	if (typeof num == 'number')
+		value.set(num);
+	else
+		throw new Error('type ' + value.type + ' increase not support');
+
+	return num;
+};
+
+/*
+ * plus
+ */
 exports.add = function(params) {
 	var sum = 0;
 	params.forEach(function(value) {
@@ -6,6 +47,9 @@ exports.add = function(params) {
 	return sum;
 };
 
+/*
+ * minus
+ */
 exports.sub = function(params) {
 	var sum = params[0].get();
 	params.slice(1).forEach(function(value) {
@@ -14,6 +58,9 @@ exports.sub = function(params) {
 	return sum;
 };
 
+/*
+ * multiply
+ */
 exports.mul = function(params) {
 	var sum = params[0].get();
 	params.slice(1).forEach(function(value) {
@@ -22,6 +69,9 @@ exports.mul = function(params) {
 	return sum;
 };
 
+/*
+ * divide
+ */
 exports.div = function(params) {
 	var sum = params[0].get();
 	params.slice(1).forEach(function(value) {
@@ -30,6 +80,9 @@ exports.div = function(params) {
 	return sum;
 };
 
+/*
+ * modulo
+ */
 exports.mod = function(params) {
 	var sum = params[0].get();
 	params.slice(1).forEach(function(value) {
